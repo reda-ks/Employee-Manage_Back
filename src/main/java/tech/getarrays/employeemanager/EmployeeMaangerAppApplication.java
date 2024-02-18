c40d145af7cc95be6ea6f9c90add6ba38c6c9009
reda-ks <redaelksis@gmail.com>
2024-02-18 16:00:11 +0100

    myfirst commit

---
+++ src/main/java/tech/getarrays/employeemanager/EmployeeMaangerAppApplication.java
@@ -0,0 +1,13 @@
+package tech.getarrays.employeemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeMaangerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeMaangerAppApplication.class, args);
+	}
+
+}
